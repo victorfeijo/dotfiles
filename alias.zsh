3a6444ec589c673c46588e49e5632ff752190d0c
Victor Feijó <victor.feijoa@gmail.com>
2016-05-08 11:08:19 -0300

    added the aliases file

---
+++ alias.zsh
@@ -0,0 +1,15 @@
+# aliases for Tmux
+alias tmux='tmux -2'
+alias ta='tmux attach -t'
+alias tnew='tmux new -s'
+alias tls='tmux ls'
+alias tkill='tmux kill-session -t'
+
+# convenience aliases for editing configs
+alias ev='vim ~/.vimrc'
+alias et='vim ~/.tmux.conf'
+alias ez='vim ~/.zshrc'
+
+# vim alias
+alias vim='nvim'
+
